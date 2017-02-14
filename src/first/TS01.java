ede61d403378f3a0ddb9dc53fa127b78be7a17fa
Julia Kozyreva <konfast@gmail.com>
2017-02-14 20:48:13 +0200

    Initial commit

---
+++ src/first/TS01.java
@@ -0,0 +1,12 @@
+package first;
+
+/**
+ * Created by Konfast on 13.02.17.
+ */
+public class TS01 {
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+    }
+
+}
